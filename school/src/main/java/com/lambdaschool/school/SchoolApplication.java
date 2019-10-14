779d36898de99b86fca6dc1c84a8068fe1963e3a
Krishan Nattar <krishan.nattar@gmail.com>
2019-10-13 18:34:00 -0700

    got endpoint working

--- school/src/main/java/com/lambdaschool/school/SchoolApplication.java
+++ school/src/main/java/com/lambdaschool/school/SchoolApplication.java
@@ -2,7 +2,10 @@ package com.lambdaschool.school;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
+//@EnableJpaAuditing
 @SpringBootApplication
 public class SchoolApplication
 {
---
+++ school/src/main/java/com/lambdaschool/school/handlers/RestExceptionHandler.java
@@ -0,0 +1,4 @@
+package com.lambdaschool.school.handlers;
+
+public class RestExceptionHandler {
+}
