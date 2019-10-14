e3350435d624c59bf42123ff6b6620475ddb92d1
Krishan Nattar <krishan.nattar@gmail.com>
2019-10-13 18:43:37 -0700

    made exception classes, but nothing is happening

--- school/src/main/java/com/lambdaschool/school/SchoolApplication.java
+++ school/src/main/java/com/lambdaschool/school/SchoolApplication.java
@@ -2,6 +2,7 @@ package com.lambdaschool.school;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableWebMvc
